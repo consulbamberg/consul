c9fbc28e14d8028ee28d2d89442bf127834a9a2d
itw-smart <tobias@demokratie.today>
2021-01-17 11:50:18 +0100

    mp

--- app/controllers/custom/proposals_controller.rb
+++ app/controllers/custom/proposals_controller.rb
@@ -1,6 +1,20 @@
 require_dependency Rails.root.join("app", "controllers", "proposals_controller").to_s
 
 class ProposalsController
+
+  before_action :authenticate_user!, except: [:index, :show, :map, :summary, :json_data]
+  
+  def all_proposal_map_locations
+    ids = if params[:search]
+      Proposal.search(params[:search]).pluck(:id)
+    elsif params[:tags]
+      Proposal.not_archived.published.tagged_with(params[:tags].split(","), all: true)
+    else
+      Proposal.not_archived.published.pluck(:id)
+    end
+    MapLocation.where(proposal_id: ids).map(&:json_data)
+  end
+
   def index_customization
     discard_draft
     discard_archived
@@ -9,13 +23,26 @@ class ProposalsController
     load_featured
     remove_archived_from_order_links
     take_only_by_tag_names
+    @proposals_coordinates = all_proposal_map_locations
+  end
+
+  def json_data
+    proposal = Proposal.find(params[:id])
+    data = {
+      proposal_id: proposal.id,
+      proposal_title: proposal.title
+    }.to_json
+
+    respond_to do |format|
+      format.json { render json: data }
+    end
   end
 
   private
-    def take_only_by_tag_names
-      if params[:tags].present?
-        @resources = @resources.tagged_with(params[:tags].split(","), all: true)
-        @subcategories = @resources.tag_counts.subcategory
-      end
+  def take_only_by_tag_names
+    if params[:tags].present?
+      @resources = @resources.tagged_with(params[:tags].split(","), all: true)
     end
+  end
+  
 end
